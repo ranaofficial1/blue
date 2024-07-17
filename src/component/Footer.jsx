33b7944034db4544802c40d39ca45da45697a045
blueminch1 <rana@blueminch.com>
2024-07-17 16:01:54 +0530

    first commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../assets/css/Footer.css'
+
+const Footer = () => {
+  return 
+    <>
+       <footer>
+         .
+       </footer>
+    </>
+  ;
+};
+
+export default Footer;
